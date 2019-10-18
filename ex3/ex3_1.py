6887b5f5de9b8684920f8c6300a549f715f09e84
Chad Guidry <chadjguidry@gmail.com>
2019-10-17 20:34:19 -0500

    Exercise 3

---
+++ ex3/ex3_1.py
@@ -0,0 +1,35 @@
+# Simple print statement
+print("I will now count my chickens:")
+
+# Prints Hens, then calculates the number of Hens
+print("Hens", 25 + 30 / 6)
+# Prints Roosters, then calculates the number of Roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Simple print statement
+print("Now I will count the eggs:")
+
+# Prints the result of a lengthy calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Simple print statement
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints the result of the calculation shown
+print(3 + 2 < 5 - 7)
+
+# Prints the sum of 3 and 2
+print("What is 3 + 2?", 3 + 2)
+# Prints the sum of 5 and -7
+print("What is 5 - 7?", 5 - 7)
+
+# Simple print statement
+print("Oh, that's why it's False.")
+
+# Simple print statement
+print("How about some more.")
+
+# Print statements with a comparison
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3/ex3_3.py
@@ -0,0 +1,4 @@
+length = 5 
+width = 17
+
+print("The area of the rectange is " + str(length * width) + ".")
