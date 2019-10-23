c15a5fdbb6cb0a03c9e746d21b659cb6cf2c8fb7
Chad Guidry <chadjguidry@gmail.com>
2019-10-22 20:33:50 -0500

    Exercise 7

---
+++ ex7/ex7_1.py
@@ -0,0 +1,32 @@
+# Prints the string "Mary had a little lamb"
+print("Mary had a little lamb.")
+
+# Prints the string "It's fleece was white as snow."
+print("It's fleece was white as {}.".format('snow'))
+
+# Prints the string "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+
+# Prints the string ..........
+print("." * 10) # what'd that do?
+
+# Assign string variables to spell Cheese Burger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# Print the string "Cheese "
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+
+# Print the string "Burger"
+print(end7 + end8 + end9 + end10 + end11 + end12)
