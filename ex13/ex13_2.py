87028a46858ed2325b16b1ce0ce4da0948655acf
Chad Guidry <chadjguidry@gmail.com>
2019-10-26 08:45:18 -0500

    Exercise 13

---
+++ ex13/ex13_2.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, name, age = argv
+
+gender = input("What is your gender? ")
+
+print(f"Your name is {name}.")
+print(f"Your age is {age}.")
+print(f"Your gender is {gender}.")
+
