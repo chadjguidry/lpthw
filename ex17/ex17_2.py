e082f7d99ed561ce203d58a852d113e843f7c76f
Chad Guidry <chadjguidry@gmail.com>
2019-11-06 21:49:10 -0600

    Exercise 17 Study Drill 2

--- ex17/ex17_2.py
+++ ex17/ex17_2.py
@@ -6,13 +6,13 @@ script, from_file, to_file = argv
 print(f"Copying {from_file} to {to_file}")
 
 # we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+# in_file = open(from_file)
+# indata = open(from_file).read()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+open(to_file, 'w').write(open(from_file).read())
+# out_file.write(indata)
 
 print("Alright, all done.")
 
-out_file.close()
-in_file.close()
+#out_file.close()
+# in_file.close()
